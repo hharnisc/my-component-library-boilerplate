805e95b0f305317ac069d6effc708dace77a356e
Harrison Harnisch <hharnisc@gmail.com>
2017-12-10 11:06:28 -0600

    add stores for configurable Text size

--- Text/story.jsx
+++ Text/story.jsx
@@ -5,4 +5,18 @@ import Text from './index';
 storiesOf('Text', module)
   .add('default', () => (
     <Text>oh hey!</Text>
+  ))
+  .add('size=3', () => (
+    <Text
+      size={3}
+    >
+      Rems = 3 = huge
+    </Text>
+  ))
+  .add('size=0.5', () => (
+    <Text
+      size={0.5}
+    >
+      Rems = 0.5 = small
+    </Text>
   ));
