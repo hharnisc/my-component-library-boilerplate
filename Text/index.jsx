4832e7a86b655f08116c0136bfa0ff99b50d9fb1
Harrison Harnisch <hharnisc@gmail.com>
2017-12-10 11:08:56 -0600

    implement configurable Text size

--- Text/index.jsx
+++ Text/index.jsx
@@ -1,3 +1,13 @@
 import React from 'react';
 
-export default ({ children }) => <span>{children}</span>;
+export default ({
+  children,
+  size=1,
+}) =>
+  <span
+    style={{
+      fontSize: `${size}rem`,
+    }}
+  >
+    {children}
+  </span>;
